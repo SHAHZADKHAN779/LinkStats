aee6ebbc23aae6efa89a94eea260402cb97e6a82
SHAHZADKHAN779 <shahzadgull779@gmail.com>
2025-09-23 22:25:39 +0500

    Index, AboutUs And ContactUs Pages Done #1

---
+++ src/LinkStats/LinkStats.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LinkStats.Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		[Route("/")]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[Route("/about-us")]
+		public IActionResult AboutUs()
+		{
+			return View();
+		}
+
+		[Route("/Home/ContactUs")]
+		public IActionResult ContactUs()
+		{
+			return View();
+		}
+	}
+}
